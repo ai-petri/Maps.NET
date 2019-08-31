8ccc284fb74fb06123a3eab78ceefa18245b22c6
Dmitry <234518@mail.ru>
2019-08-31 12:47:19 +0200

    RaisePropertyChanged

--- Maps/ViewModels/ViewModel.cs
+++ Maps/ViewModels/ViewModel.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,5 +20,11 @@ namespace Maps.ViewModels
         public Model Model { get; private set; }
 
         public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
