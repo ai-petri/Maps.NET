6739f6e8f1a253aec735f7d39be1a7b20eed74f6
Dmitry <234518@mail.ru>
2019-09-06 07:28:08 +0300

    добавлен класс Location

---
+++ Maps/Location.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maps
+{
+    public class Location
+    {
+        public Location(string name, double longitude, double latitude)
+        {
+            Name = name;
+            Longitude = longitude;
+            Latitude = latitude;
+        }
+
+        public string Name { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+
+        public Dictionary<string, double> Values { get; set; }
+    }
+}
